da8aafd363835d87a7959e69fc729ed644344435
w_duse89 <wyatt.duesenberg@gmail.com>
2019-10-30 13:31:55 -0600

    sumStuff

--- server.js
+++ server.js
@@ -27,29 +27,33 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.politico.com/news/2020-elections").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("article").each(function(i, element) {
       // Save an empty result object
       var result = {};
+      console.log(element);
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
+        .find("a")
         .text();
       result.link = $(this)
-        .children("a")
+        .findf("a")
         .attr("href");
+      result.img = $(this)
+        .find("img")
+        .text()
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
